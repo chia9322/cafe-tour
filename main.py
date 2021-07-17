69491dfda4c30ee593d911640d697ec70937686a
chia <chia9322@gmail.com>
2021-07-17 16:36:49 +0900

    Replace DATABASE_URL postgres to postgresql

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import os
 app = Flask(__name__)
 
 # ------------------- DATABASE ------------------- #
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///cafes.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL?sslmode=require", "sqlite:///cafes.db")
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
